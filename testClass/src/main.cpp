64c03bda08a4f3358def8c4783e1b0eaed437714
myname <my@example.com>
2017-09-20 09:53:40 +0800

    something change

--- testClass/src/main.cpp
+++ testClass/src/main.cpp
@@ -1,6 +1,9 @@
 #include "../inc/Rectangle.h"
 #include "../inc/Triangle.h"
 
+#include <iostream>
+using namespace std;
+
 int main(int args,char* argv[]){
 	Rectangle *r = Rectangle::getInstance();
 	r->show();
@@ -8,6 +11,17 @@ int main(int args,char* argv[]){
 	Triangle *t = new Triangle();
 	t->show();
 	delete t;
+	if(0){
+		#define IST_HELLO "hello"
+	}
+	#if defined(IST_HELLO)
+		cout<<IST_HELLO<<" " << args<<endl;
+	#endif
+	#if defined(IST_10003_86S)
+		cout<< " defined IST_10003_86S "<<endl;
+	#else
+		cout<<" haven't defined IST_10003_86S "<<endl;
+	#endif
 	return 0;
 }
 
