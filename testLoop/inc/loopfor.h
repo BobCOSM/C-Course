ae11ae37bc4211d331497760d404863d4fdf33af
LinuxBOB <linuxbob@linuxbob-virtual-machine.(none)>
2018-07-02 19:47:58 +0800

    add bubble sort and select sort

--- testLoop/inc/loopfor.h
+++ testLoop/inc/loopfor.h
@@ -2,5 +2,7 @@
 #define __LOOPFOR_H__
 
 void printnnt(); // 9x9 mul
+void bubble_sort(int *nums,int size);
+void select_sort(int *nums,int size);
 
 #endif
--- testLoop/src/loopfor.c
+++ testLoop/src/loopfor.c
@@ -14,3 +14,59 @@ void printnnt() //9x9 mul table
         printf("\n");
     }
 }
+
+static void print_nums(int *nums,int size)
+{
+    int i = 0;
+    for(i = 0;i < size; i++)
+    {
+        printf("%d ",nums[i]);
+    }
+    printf("\n");
+}
+
+void bubble_sort(int *nums,int size)
+{
+    int i = 0;
+    for(i = 0;i < size; i++)
+    {
+        int j = 0;
+        for(j = 0; j < size - i - 1; j++)
+        {
+            if(nums[j] < nums[j+1])
+            {
+                //int tmp_num = nums[j];
+                nums[j] = nums[j + 1] + nums[j];
+                nums[j+1] = nums[j] - nums[j + 1];
+                nums[j] = nums[j] - nums[j + 1];
+            }
+        }
+    }
+    print_nums(nums,size);
+}
+
+void select_sort(int *nums,int size)
+{
+    int i = 0;
+    for(i = 0; i < size; i++)
+    {
+        int key_index = i;
+        int j = 0;
+        for(j = i + 1; j < size; j++)
+        {
+            if(nums[key_index] < nums[j])
+            {
+                key_index = j;
+            }
+        }
+        //printf("%d ",nums[key_index]);
+        if(key_index != i)
+        {
+            nums[key_index] = nums[key_index] + nums[i];
+            nums[i] = nums[key_index] - nums[i];
+            nums[key_index] = nums[key_index] - nums[i];
+        }
+    }
+    print_nums(nums,size);
+}
+
