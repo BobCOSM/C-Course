ea0ebe7f9f535b216210b3ce6df95e7a2ff62b65
bob_captain <bob_captain@163.com>
2018-07-25 12:44:49 +0800

    copy contribute test

--- testC++Grammer/main.cpp
+++ testC++Grammer/main.cpp
@@ -5,50 +5,64 @@ class A
 private:
     int data;
 public:
-    A()
-    {
-        ;
-    }
+    A();
+    int get_data();
+    bool equal(A *det);
     A(int data);
     A(const A&);
-    int get_data()
-    {
-        return data;
-    }
-    bool equal(A det){
-        //cout << "this " << &this <<endl;
-        cout << "det " << &det <<endl;
-        return data == det.get_data();
-    }
 };
+
+A::A()
+{
+}
+int A::get_data()
+{
+    return data;
+}
+bool A::equal(A* det){
+    //cout << "this " << &this <<endl;
+    cout << "det " << det <<endl;
+    return data == det->get_data();
+}
 
 A::A(int data)
 {
+    cout << "a contribute " <<endl;
     this->data = data;
 }
 A::A(const A &det)
 {
-    data = 200;
+    cout << "a copy contribute " <<endl;
+    data = det.data;
 }
 class B
 {
 private:
-    A data;
+    A* data;
 public:
-    B(const A data);
-    A get_data()
+    B(A* data);
+    A* get_data();
+    bool equal(B* det);
+
+    ~B()
     {
-        return data;
-    }
-    bool equal(B det)
-    {
-        return data.equal(det.get_data());
+        delete data;
     }
 };
 
-B::B(const A data)
+A* B::get_data()
 {
-    this->data = data;
+    return data;
+}
+
+bool B::equal(B* det)
+{
+    return data->equal(det->get_data());
+}
+
+B::B(A* data)
+{
+    this->data = new A(*data);
 }
 
 template<typename T>
@@ -72,12 +86,12 @@ int sum(double a,int b)
 int main()
 {
     int _sum = sum(10.0,20);
-    A a0 = A(100);
-    A a1 = A(200);
-    bool res0 = a0.equal(a1);
+    A *a0 = new A(100);
+    A *a1 = new A(200);
+    bool res0 = a0->equal(a1);
 
-    cout << a0.get_data() <<endl;
-    cout << a1.get_data() <<endl;
+    cout << a0->get_data() <<endl;
+    cout << a1->get_data() <<endl;
     if(res0)
     {
         cout << "a0 == a1" <<endl;
@@ -87,14 +101,14 @@ int main()
         cout << "a0 != a1" <<endl;
     }
 
-    B b0 = B(a0);
-    B b1 = B(a1);
-    B b2 = B(a0);
+    B* b0 = new B(a0);
+    B* b1 = new B(a1);
+    B* b2 = new B(a0);
 
     cout << "second " << endl;
-    cout << a0.get_data() <<endl;
-    cout << a1.get_data() <<endl;
-    bool res1 = b1.equal(b0);
+    cout << a0->get_data() <<endl;
+    cout << a1->get_data() <<endl;
+    bool res1 = b1->equal(b0);
 
     if(res1)
     {
@@ -105,7 +119,7 @@ int main()
         cout << "b0 != b1" <<endl;
     }
 
-    bool res2 = b2.equal(b0);
+    bool res2 = b2->equal(b0);
     if(res2)
     {
         cout << "b0 == b2" <<endl;
